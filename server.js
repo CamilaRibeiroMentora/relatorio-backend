5fde432704f7509a8c09e1bd6cb620a439c63235
CamilaRibeiroMentora <camilaribeiro648@gmail.com>
2026-05-19 21:31:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 
 async function chamarClaude(mensagens, maxTokens) {
